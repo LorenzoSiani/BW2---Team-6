60cacb660c974f34556118fab04b3704c87cde60
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-07-11 11:40:18 +0200

    artist page 3

--- ArtistPage/artistScritp.js
+++ ArtistPage/artistScritp.js
@@ -24,8 +24,7 @@ fetch(artistUrl //+ artistId//
     const divArtist = document.getElementById('conteiner-artist')
     divArtist.innerHTML = `
     
-    <div>
-      <img src="${detail.picture_big}" alt="" />
+    <div id="background-img" style="background-image: url('${detail.picture_big}')">
       <h3>${detail.name}</h3>
     </div>
     <div>
@@ -33,17 +32,17 @@ fetch(artistUrl //+ artistId//
       <div class="d-flex justify-content-between align-items-center">
         <div>
           <button>Seguiti</button
-          ><i class="bi bi-three-dots-vertical text-white"></i>
+          >
         </div>
-        <div
-          class="d-flex justify-content-evenly align-items-center w-25 fs-1 mx-3"
-        >
+        <div class="comands d-flex justify-content-between align-items-center">
+        <div class="d-flex justify-content-between w-25 fs-3 mx-4">
+          <i class="bi bi-three-dots-vertical text-secondary"></i>
+        </div>
+        <div class="d-flex justify-content-evenly align-items-center w-25 fs-1 mx-3">
           <i id="shuffle" class="bi bi-shuffle text-secondary"></i>
-          <i
-            id="play-button"
-            class="bi bi-play-circle-fill text-success mx-3"
-          ></i>
+          <i id="play-button" class="bi bi-play-circle-fill text-success mx-3"></i>
         </div>
+      </div>
       </div>
     </div>
     <div class="d-flex justify-content-start">
@@ -59,6 +58,28 @@ fetch(artistUrl //+ artistId//
     <div id="tracklist-conteiner" class="d-flex flex-column">
     </div>
     `
+
+    const play = document.getElementById('play-button');
+    play.addEventListener('click', function () {
+      if (play.classList.contains('bi-play-circle-fill')) {
+        play.classList.remove('bi-play-circle-fill');
+        play.classList.add('bi-pause-circle-fill');
+      } else {
+        play.classList.remove('bi-pause-circle-fill');
+        play.classList.add('bi-play-circle-fill');
+      }
+    });
+
+    const shuffle = document.getElementById('shuffle');
+    shuffle.addEventListener('click', function () {
+      if (shuffle.classList.contains('bi-shuffle') && shuffle.classList.contains('text-secondary')) {
+        shuffle.classList.remove('text-secondary');
+        shuffle.classList.add('text-light');
+      } else {
+        shuffle.classList.remove('text-light');
+        shuffle.classList.add('text-secondary');
+      }
+    });
     fetch(detail.tracklist)
     .then((res)=>{
         if(res.ok){
@@ -78,7 +99,7 @@ fetch(artistUrl //+ artistId//
             
             <div class="d-flex my-3 align-items-center">
               <div class="mx-2">${i + 1}</div>
-              <div><img id="artist-img" src="${el.album.cover}" alt="" /></div>
+              <div class="me-2"><img id="song-img" src="${el.album.cover}" alt="" /></div>
               <div>
                 <h5>${el.title}</h5>
                 <p>${el.rank}</p>
