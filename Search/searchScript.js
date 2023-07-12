01757d1681d6e8440895a448a3d73c7a4e5c11a6
Vincenzo Cicero <cicero.vinz@gmail.com>
2023-07-12 17:30:32 +0200

    commit
    prova

--- Search/searchScript.js
+++ Search/searchScript.js
@@ -23,13 +23,38 @@ inputSearch.addEventListener('submit', function(e){
             let newDiv = document.createElement('div')
             
             newDiv.innerHTML=`
-            <div class="card">
-               <img src="${el.album.cover_medium}" class="card-img-top" alt="...">
-               <div class="card-body">
-                 <a href="http://127.0.0.1:5500/AlbumPage/album.html?id=${el.album.id}" class="btn btn-primary">${el.album.title}</a>
-                 <a href="http://127.0.0.1:5500/ArtistPage/artist.html?id=${idArtist}" class="btn btn-primary">${el.artist.name}</a>
-               </div>
+
+
+            <div class="container d-flex justify-content-center">
+      
+            <div class="col-11 col-sm-11 col-md-4 d-flex align-items-center mt-3">
+              <div class="d-flex align-items-center ">
+              <img style=" width: 40%; max-width: 280px; border-style: none; margin-right: 10px;
+            "src="${el.album.cover_medium}" class="card-img img-thumbnail" alt="immagine-Album">
+              <div class="d-flex flex-column ">
+              <a onclick="window.location.href='http://127.0.0.1:5500/AlbumPage/album.html?id=${el.album.id}'" >
+              <h5 class="card-title">${el.album.title}</h5></a>
+              
+              <a onclick="window.location.href='http://127.0.0.1:5500/ArtistPage/artist.html?id=${idArtist}'" >
+              
+              <p class="card-text link-secondary">${el.artist.name}</p></a>
+            
+                </div>
+              </div>
             </div>
+            <div class="col-1 col-sm-1 col-md-8 d-flex align-items-center justify-content-end">
+              <div class="d-flex align-items-center d-none d-lg-flex">
+                
+               
+              </div>
+              <div class="d-flex  align-items-center">
+                <i class="bi bi-three-dots-vertical text-secondary"></i>
+              </div>
+            </div>
+          </div>
+         
+
+
             `
             newCont.appendChild(newDiv)
         });
