0ba50fbc4307386caeaa5c6a9544f07e1bd52208
ValerioParatore <ValerioTeoParatore@gmail.com>
2023-07-13 15:47:17 +0200

    days 4 si prega

--- Homepage/homePageScript.js
+++ Homepage/homePageScript.js
@@ -1,4 +1,4 @@
-const albumsArray = [12047952,8887733,1345688,9536122,1121181,303415,68346981,92071,70200002,212377]
+const albumsArray = [85363,62646342,1434890,1434890,92071,397544257,51001312,8887733,1345688,9536122,1121181,303415,68346981,70200002,212377]
 const albumUrl = 'https://striveschool-api.herokuapp.com/api/deezer/album/'
 const albumCont = document.getElementById('album-container')
 const albumCont2 = document.getElementById('album-container-2')
@@ -18,22 +18,13 @@ const homePageRend = function(){
             let newDiv = document.createElement('div')
             newDiv.classList.add('col')
             newDiv.innerHTML=`
-            
-
-      <div class="card mainCards">
-        <div class="card-body">
-          <div>
-            <div>
-            <img src="${detail.cover_medium}"  alt="Immagine" width="150px"/>
-              <div class="right-div">
-              <a href="http://127.0.0.1:5500/AlbumPage/album.html?id=${detail.id}" class="text-light">${detail.title}</a>
-              <a href="http://127.0.0.1:5500/ArtistPage/artist.html?id=${detail.artist.id}" class="text-light">${detail.artist.name}</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-            
+            <div class="card img-card p-2">
+              <img src="${detail.cover_medium}" class="card-img-top" alt="...">
+               <div class="card-body">
+               <a href="http://127.0.0.1:5500/AlbumPage/album.html?id=${detail.id}" class="text-light f-4 fw-medium">${detail.title}</a>
+               <a href="http://127.0.0.1:5500/ArtistPage/artist.html?id=${detail.artist.id}" class="text-light f-6 fw-lighter">${detail.artist.name}</a>
+               </div>
+            </div>            
             `
             albumCont.appendChild(newDiv)
         })
@@ -65,13 +56,13 @@ const homePageRend2 = function(){
         <div>
           <div class="d-flex">
           <img src="${detail.cover_medium}" alt="Immagine" width="150px" />
-            <div class="right-div">
+            <div class="right-div ms-3">
             <a href="http://127.0.0.1:5500/AlbumPage/album.html?id=${detail.id}" class="text-light">${detail.title}</a>
             <a href="http://127.0.0.1:5500/ArtistPage/artist.html?id=${detail.artist.id}" class="text-light">${detail.artist.name}</a>
             </div>
           </div>
-          <div class="text-light d-flex mt-2">
-            <div><i class="bi bi-heart"></i><i class="bi bi-three-dots-vertical"></i><div/><div><p>${detail.tracks.data.length}brani</p><i class="bi bi-play-circle"></i></div>
+          <div class="text-light d-flex mt-2 align-items-center justify-content-between">
+            <div><i class="bi bi-heart fs-3 p-2"></i><i class="bi bi-three-dots-vertical fs-3 p-2"></i></div><div class="d-flex align-items-center"><p>${detail.tracks.data.length}brani</p><i class="bi bi-play-circle fs-3 p-2"></i></div>
           </div>
         </div>
       </div>
