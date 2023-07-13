46df1050cab54576f4df3016ad5bbc9b34e9846f
Vincenzo Cicero <cicero.vinz@gmail.com>
2023-07-13 16:36:13 +0200

    tasto search ok

--- Search/searchScript.js
+++ Search/searchScript.js
@@ -81,26 +81,21 @@ function getRandomColor() {
   });
 
 
-//   PROVO A RANDOMIZZARE LE INSIDE IMG
+// RIMUOVI ICONA DA SEARCBOX
 
-//   const images = [
-//     "image-6.jpg",
-//     "image-2.jpg",
-//     "image-3.jpg",
-//     "image-4.jpg",
-//     "image-5.jpg",
-//     "image-7.jpg",
-//     "image-8.jpg",
-//     "image-9.jpg",
-//     "image-10.jpg",
-//     "image-12.jpg"
-//   ];
-  
-//   const randomIndex = Math.floor(Math.random() * images.length);
-//   const randomImage = "assets/imgs/main" + images[randomIndex];
-  
-//   const imgElement = document.createElement("img");
-//   imgElement.src = randomImage;
-  
-//   const imageContainer = document.querySelectorAll(".image-container");
-//   imageContainer.appendChild(imgElement);
+const searchInput = document.querySelector('.sbx-custom__input');
+const searchIcon = document.querySelector('.sbx-custom__submit svg');
+
+searchInput.addEventListener('input', function() {
+  if (this.value.trim() !== '') {
+    searchIcon.style.display = 'none';
+  } else {
+    searchIcon.style.display = '';
+  }
+});
+
+searchInput.addEventListener('keyup', function(event) {
+  if (event.code === 'Backspace' && this.value.trim() === '') {
+    searchIcon.style.display = '';
+  }
+});
